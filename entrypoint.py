184b2b95f4a4029f24180a4d8ee1a3cd1c1866fe
camilusa <camilo.rodriguezt@usa.edu.co>
2024-03-09 12:35:53 +0000

    Add preprocessing model with EMR-spark

--- entrypoint.py
+++ entrypoint.py
@@ -1,5 +1,13 @@
 import joblib
 import os
+
 def model_fn(model_dir):
     clf = joblib.load(os.path.join(model_dir, "model.joblib"))
     return clf
+
+def predict_fn(input_data, model):
+     if len(input_data.shape) == 1:
+      proba = model.predict_proba(input_data.reshape(-1, 1).T)
+     else:
+      proba = model.predict_proba(input_data)   
+     return proba
